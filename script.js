a76576435825072789d040beb84929b009611789
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2021-05-14 16:53:53 +0530

    Starting steps to create GIF search engine

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-for(i=0; i<20; i++) {
-    setTimeout(() => {
-        var box = document.createElement('div');
-				box.className = 'box';
-				document.getElementById('container').appendChild(box);
-    }, 500*i);
-}
+// Grab the input
+
+
+//Getting input form API
+
+
+//Showing GIFs
