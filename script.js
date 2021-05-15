762c5add0df36594d438037f814a3195b250007e
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2021-05-15 16:49:43 +0530

    js updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 document.querySelector(".js-go").addEventListener('click', function() {
 
     var input = document.querySelector("input").value;
-    pushToDOM(input);
+    searchTerm(input);
 
 });
 
@@ -13,35 +13,36 @@ document.querySelector(".js-userinput").addEventListener('keyup', function(e) {
 
     // if the key "enter" is pressed...
     if(e.which == 13) {
-    pushToDOM(input);
+    searchTerm(input);
     }
 
 });
 
-
-
+function searchTerm(input) {
 
 
 //Getting input form API
 
-var url = "http://api.giphy.com/v1/gifs/search?q=sri+lanka&api_key=ULQF8JPwR4gD5R3LXPi3ehqdkAG5wkfE";
+var url1 = "http://api.giphy.com/v1/gifs/search?q="
+var url2 = input;
+var url3 = "&api_key=ULQF8JPwR4gD5R3LXPi3ehqdkAG5wkfE";
+var fullUrl = url1+url2+url3;
+
 
 // AJAX Request
 var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open( 'GET', url );
+GiphyAJAXCall.open( 'GET', fullUrl );
 GiphyAJAXCall.send();
 
 GiphyAJAXCall.addEventListener('load', function(e) {
 
     var data = e.target.response;;
     pushToDOM(data);
-})
-
-
+});
+}
 //Showing GIFs
 
 function pushToDOM(input) {
-
     var response = JSON.parse(input);
 
     var imageUrls = response.data;
